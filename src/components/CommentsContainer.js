3368964d7b927d2b76254ec715a8e9964b73c201
Priyank Singh <priyank-singh@example.com>
2025-04-20 20:52:41 +0530

    Initial commit

---
+++ src/components/CommentsContainer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import CommentsList from "./CommentsList";
+
+const commentsData = [
+  {
+    name: "Daksh Pandey",
+    text: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium",
+    replies: [
+      {
+        name: "Daksh Pandey",
+        email: "daksh123@.com",
+        text: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+        replies: [
+          {
+            name: "quo vero reiciendis velit similique earum",
+            email: "daksh123@.com",
+            text: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+            replies: [
+              {
+                name: "quo vero reiciendis velit similique earum",
+                email: "Jayne_Kuhic@sydney.com",
+                text: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name:"Daksh Pandey",
+    email: "daksh123@.com",
+    text: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+  },
+  {
+    name: "quo vero reiciendis velit similique earum",
+    email: "Jayne_Kuhic@sydney.com",
+    text: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+  },
+  {
+    name: "quo vero reiciendis velit similique earum",
+    email: "Jayne_Kuhic@sydney.com",
+   text: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+    replies: [
+      {
+        name: "quo vero reiciendis velit similique earum",
+        email: "Jayne_Kuhic@sydney.com",
+       text: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+        replies: [
+          {
+            name: "quo vero reiciendis velit similique earum",
+            email: "Jayne_Kuhic@sydney.com",
+           text: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const CommentsContainer = () => {
+  return (
+    <div>
+      <h1 className="text-lg font-medium mb-2">Comments</h1>
+      <CommentsList comments={commentsData} />
+    </div>
+  );
+};
+
+export default CommentsContainer;
+
